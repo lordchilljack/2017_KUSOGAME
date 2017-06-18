a7bc97f36ce1bd623817ee9819929e87726a2ebc
lordchilljack <lordchilljack@gmail.com>
2017-06-18 18:02:32 +0800

    alpha

--- Attack on Thesis/Assets/Script/game.cs
+++ Attack on Thesis/Assets/Script/game.cs
@@ -6,14 +6,16 @@ using UnityEngine.UI;
 
 public class game : MonoBehaviour {
     public double hp1 = 100;
-    public double hpy = 100;
+    public double hpy = 87;
     public double work = 1440;
     public double paper = 0;
     public float time = 0;
     public int tt;
+	public double Lday=60;
 
-    public Text hpText, hpyText,worktext,papertext,day1;
+	public Text hpText, hpyText,worktext,papertext,day1,Ldaytext;
     public Button b1,b2,b3;
+	private int HPboost = 0;
     Animator aime;
     public void Start()
     {
@@ -31,6 +33,8 @@ public class game : MonoBehaviour {
         paper += (100 - hp1) / 100;
         papertext.text = (paper).ToString();
         aime.SetTrigger("do1");
+		HPboost = 0;
+		aime.SetBool("ButtonPushed",true);
 
     }
     public void Down2()
@@ -41,6 +45,8 @@ public class game : MonoBehaviour {
         worktext.text = (work).ToString();
         hpy = hpy + 0.1;
         hpyText.text = (hpy ).ToString();
+		HPboost = 0;
+		aime.SetBool("ButtonPushed",true);
     }
 
     public void Down3()
@@ -51,19 +57,35 @@ public class game : MonoBehaviour {
         worktext.text = (work).ToString();
         hpy = hpy + 0.5;
         hpyText.text = (hpy ).ToString();
+		HPboost = 0;
+		aime.SetBool("ButtonPushed",true);
     }
     
     public void Update()
     {
         time += Time.deltaTime;
         day1.text = (time).ToString();
-        
-        if ((int)time>tt)
+		        if ((int)time>tt)
         {
+			HPboost++;
             work = work - 1;
             worktext.text = (work).ToString();
-            hp1 = hp1 + 1;
+			if (hp1 < 100) 
+			{
+				if (HPboost >5&&hp1<90)
+				{
+					aime.SetBool("ButtonPushed",false);
+					hp1 = hp1 + 10;
+				}
+				else
+				{
+					hp1 = hp1 + 0.5;
+				}
+
+			}
             hpText.text = hp1.ToString();
+			Lday=work/24;
+			Ldaytext.text = (Lday).ToString();
         }
         tt = (int)time;
     }
